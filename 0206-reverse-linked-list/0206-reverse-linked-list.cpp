6803ad2a0e6dd7ff57e07f02e39735275feb3b11
Sumendra Prajapati <136796249+Sumendra166@users.noreply.github.com>
2026-04-08 10:31:35 +0530

    Time: 0 ms (100%), Space: 13.3 MB (71.69%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,19 @@
+// 75 DAYS LEETCODE CHALLENGE  Day 28
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+
+        while (curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
